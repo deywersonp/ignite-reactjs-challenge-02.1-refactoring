7e2da687c31385a74b504c02728adf0c4bd15c36
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-05-16 19:35:27 -0300

    refactored Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,45 @@
+import ReactModal from 'react-modal';
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: () => void;
+};
+
+function Modal({
+  children,
+  isOpen,
+  setIsOpen,
+}: Props) {
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={isOpen}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
